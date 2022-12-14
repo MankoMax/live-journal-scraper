fa2c77ea335849a0c42cd023b1ac540867d2f826
MankoMax <fanatscape@gmail.com>
2022-12-14 12:54:50 +0200

    now working with .docx

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dotenv import dotenv_values, load_dotenv
 load_dotenv()
 
 CALENDAR_PAGE = os.getenv("CALENDAR_PAGE")
+TEST_LINKS = ["https://navalny.livejournal.com/209488.html", "https://navalny.livejournal.com/209963.html", "https://navalny.livejournal.com/616157.html"]
 
 
 class Main(Scrapper):
@@ -13,16 +14,13 @@ class Main(Scrapper):
         years_links = self.get_calendar_years()
         print(len(years_links))
 
-        month_links = self.get_calendar_month(years_links)
-        print(len(month_links))
-
-        days_links = self.get_calendar_days(month_links)
+        days_links = self.get_calendar_days(years_links)
         print(len(days_links))
 
         posts_links = self.get_posts_links(days_links)
         print(len(posts_links))
 
-        for link in posts_links:
+        for link in TEST_LINKS:
             self.save_post(link)
             
 Main(CALENDAR_PAGE).main()
--- scraper.py
+++ scraper.py
@@ -6,13 +6,20 @@ from urlhelpers import cut_link, post_from_link
 from fake_useragent import UserAgent
 from dotenv import load_dotenv
 import os
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver import Keys
+from docx import Document
 
 load_dotenv()
 
 HTTP_PROXY = os.getenv("HTTP_PROXY")
 HTTPS_PROXY = os.getenv("HTTPS_PROXY")
 
-class Scrapper:
+class Scrapper():
+    
     
     def __init__(self, calendar_page):
         self.calendar_page = calendar_page
@@ -26,11 +33,19 @@ class Scrapper:
             'http': HTTP_PROXY,
             'https': HTTPS_PROXY,
             } 
-
+        
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--no-sandbox')
                 
+        service = Service(executable_path='D:\chromedriver.exe')
+        self.driver = webdriver.Chrome(service=service, options=options)
+                 
+                        
     def get_calendar_years(self):
         urls = []
-        req = requests.get(self.calendar_page, proxies=self.proxies, headers=self.headers)
+        req = requests.get(self.calendar_page, headers=self.headers, proxies=self.proxies)
         soup = BeautifulSoup(req.text, "html.parser")
         text = soup.select("div.entry-text")
         for i in text:
@@ -41,73 +56,70 @@ class Scrapper:
         return urls
 
 
-    def get_calendar_month(self, urls_list):
-        urls = []
-        for i in urls_list:
-            req = requests.get(i, proxies=self.proxies, headers=self.headers)
-            soup = BeautifulSoup(req.text, "html.parser")
-            text = soup.select("caption")
-            for i in text:
-                for link in i.find_all('a', class_="month", id=False):
-                    res = link.get('href')
-                    if res not in urls:
-                        urls.append(res)
-        return urls
-
-
     def get_calendar_days(self, urls_list):
         urls = []
         for i in urls_list:
-            req = requests.get(i, proxies=self.proxies, headers=self.headers)
-            soup = BeautifulSoup(req.text, "html.parser")
-            text = soup.select("dt")
-            for i in text:
-                for link in i.find_all('a'):
-                    res = link.get('href')
-                    if res not in urls:
-                        urls.append(res)
+            self.driver.get(i)
+            calendars = self.driver.find_element(By.CSS_SELECTOR, value="div.entry-text")
+            bodies = calendars.find_elements(By.TAG_NAME, value="tbody")
+            for body in bodies:
+                days = body.find_elements(By.TAG_NAME, value="a")
+                days = [day.get_attribute("href") for day in days]
+                for day in days:
+                    if day not in urls:
+                        urls.append(day)
         return urls
 
 
     def get_posts_links(self, urls_list):
         urls = []
         for i in urls_list:
-            req = requests.get(i, proxies=self.proxies, headers=self.headers)
-            soup = BeautifulSoup(req.text, "html.parser")
-            text = soup.select("dt.entry-title")
-            for i in text:
-                for link in i.find_all('a'):
-                    res = link.get('href')
-                    if res not in urls:
-                        urls.append(res)
+            self.driver.get(i)
+            posts = self.driver.find_elements(By.CLASS_NAME, value="subj-link")
+            posts = [post.get_attribute("href") for post in posts]
+            for post in posts:
+                if post not in urls:
+                    urls.append(post)
         return urls
 
 
     def save_post(self, url):
-        path = os.makedirs(f'{os.path.abspath(os.curdir)}\posts\{post_from_link(url)}')
         count = 0
-        req = requests.get(url, proxies=self.proxies, headers=self.headers)
+        dir = f"posts/{post_from_link(url)}"
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+        req = requests.get(url, headers=self.headers, proxies=self.proxies)
         soup = BeautifulSoup(req.text, "html.parser")
-        headings = soup.find_all('h1')
-        texts = soup.find_all('div', class_="b-singlepost-bodywrapper", id=False)
-
-        with open(f"posts/{post_from_link(url)}/{post_from_link(url)}.txt", mode='w', encoding="utf-8") as f:
-
-            for heading in headings:
-                f.write(f'{heading.get_text()}\n')
-
-            for text in texts:
-                f.write(f'\n{text.get_text()}')
-                
-        text = soup.select("div.b-singlepost-bodywrapper")
-        
-        for i in text:
+        heading = soup.find('h1')
+        text = soup.find('div', class_="b-singlepost-bodywrapper")
+        aricle = soup.select("div.b-singlepost-bodywrapper")
+            
+        for i in aricle:
             for link in i.find_all('img'):
                 count += 1
                 res = link.get('src')
-                print(res)
-                if '.png' or '.jpg' in res:
+                if '.png' in res:
                     img = Image.open(requests.get(res, stream = True).raw)
-                    img.save(f"posts/{post_from_link(url)}/{count}.png")
-        print(f'Post {url} is saved!')
-    
+                    img.save(f"posts/{post_from_link(url)}/{count+1}.png")
+                if '.jpg' in res:
+                    img = Image.open(requests.get(res, stream = True).raw)
+                    img.save(f"posts/{post_from_link(url)}/{count+1}.jpg")
+        
+        document = Document()
+        try:
+            document.add_heading(f'{heading.get_text()}\n', level=1)
+        except AttributeError or TypeError:
+            pass
+        try:
+            document.add_paragraph(text.get_text(separator="\n") + f"\n\nСсылка на пост - {url}")
+        except AttributeError or TypeError:
+            pass
+
+        document.save(f"posts/{post_from_link(url)}/post.docx")
+        print(f"Пост сохранен - {url}")
+        
+        
+        
+        
+        
+        
